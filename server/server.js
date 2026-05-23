3b065200bd678f596f2edbe9ef4688ae4639e8ca
naahe25 <naahebd25@gmail.com>
2026-05-23 06:20:08 +0600

    Now running perfectly

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   // fallback for SPA routes
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
