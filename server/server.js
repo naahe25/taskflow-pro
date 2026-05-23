cfa373069b341b402caf5a7fc1c952fa0518e3d8
naahe25 <naahebd25@gmail.com>
2026-05-23 07:11:02 +0600

    fixed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const express = require("express");
+
 const dotenv = require("dotenv");
+
 const cors = require("cors");
+
 const morgan = require("morgan");
+
 const passport = require("passport");
+
 const cookieSession = require("cookie-session");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-const statsRoutes = require("./routes/statsRoutes");
-const teamProgressRoutes = require("./routes/teamProgressRoutes");
+
 const path = require("path");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+const projectRoutes = require("./routes/projectRoutes");
+
+const taskRoutes = require("./routes/taskRoutes");
+
+const statsRoutes = require("./routes/statsRoutes");
+
+const teamProgressRoutes = require("./routes/teamProgressRoutes");
+
 const userRoutes = require("./routes/userRoutes");
+
 const inviteRoutes = require("./routes/inviteRoutes");
 
 dotenv.config();
@@ -24,11 +38,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  "/uploads",
+
+  express.static(path.join(__dirname, "uploads")),
+);
 
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
+
     credentials: true,
   }),
 );
@@ -38,31 +59,44 @@ app.use(morgan("dev"));
 app.use(
   cookieSession({
     name: "session",
+
     keys: ["taskflowpro"],
+
     maxAge: 24 * 60 * 60 * 1000,
   }),
 );
 
 app.use(passport.initialize());
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 app.use("/api/auth", authRoutes);
+
 app.use("/api/projects", projectRoutes);
+
 app.use("/api/tasks", taskRoutes);
+
 app.use("/api/stats", statsRoutes);
+
 app.use("/api/team-progress", teamProgressRoutes);
+
 app.use("/api/users", userRoutes);
+
 app.use("/api/invites", inviteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  // fallback for SPA routes (use middleware to avoid path-to-regexp issues)
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+
+        "../client",
+
+        "dist",
+
+        "index.html",
+      ),
+    );
   });
 }
 
